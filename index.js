9993b599ede90b3142c13ef56753112ff061bb1a
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-29 11:34:48 +0530

    fetch all quote to page

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function App(){
+
+    const [quotes,setQuotes] = React.useState([]);
+    const [randomQuote,setRandomQuote] = React.useState([]);
+
+    React.useEffect(() => {
+        async function fetchData(){
+            const response = await fetch("https://type.fit/api/quotes");
+            const data = await response.json();
+
+            setQuotes(data);
+            let randomIndex = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randomIndex]);
+        }
+        fetchData();
+    },[]);
+
+
     return(
-        <div>Hello World!</div>
+        <div>Hello World!
+
+            {quotes.map(quote => (
+                <div>{quote.text}</div>
+            ))}
+        </div>
     );
 }
 
